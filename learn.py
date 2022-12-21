11a23e032bae824c701afa5909207c6a78ce5217
Massimo Bottelli <88494940+massimobottelli@users.noreply.github.com>
2022-12-21 19:29:49 +0100

    Add files via upload

--- learn.py
+++ learn.py
@@ -10,44 +10,41 @@ from sklearn.model_selection import cross_val_score
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
+print("Training model...")
 df = pd.read_csv("dataset.csv", header=0)
-
 X = df[['ball_y', 'ball_angle']]
 y = df['paddle_y']
 
+# evaluate model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-print("Training model...")
 spline_model = Pipeline([('spline', RandomForestRegressor(n_estimators=800))])
-
 spline_model.fit(X_train, y_train)
 y_pred = spline_model.predict(X_test)
-
 scores = cross_val_score(spline_model, X, y, cv=5)
 print("Cross-validation scores:", scores)
 print("Mean score:", np.mean(scores))
 print("Standard deviation:", np.std(scores))
 
-# select sample angle and draw diagram sample vs model
-set_ball_angle = 0
-set_ball_y = list(range(90, 270, 5))
+# select ball position and draw diagram sample vs model
+set_ball_y = 0
+set_ball_angle = list(range(90, 270, 5))
 predictions = []
-for value in set_ball_y:
-    test_sample = [[set_ball_angle, value]]
+for value in set_ball_angle:
+    test_sample = [[set_ball_y, value]]
     prediction = spline_model.predict(test_sample)[0]
     predictions.append(prediction)
-
-df = df[df["ball_y"] == set_ball_angle]
+df = df[df["ball_y"] == set_ball_y]
 df.plot(x='ball_angle', y='paddle_y', kind='scatter', color="orange", label="dataset")
-plt.plot(set_ball_y, predictions, label="prediction")
-plt.title("Spline prediction vs dataset (ball-y = "+str(set_ball_angle)+")")
+plt.plot(set_ball_angle, predictions, label="prediction")
+plt.title("Spline prediction vs dataset (ball-y = "+str(set_ball_y)+")")
 plt.legend()
-plt.xlim(140, 220)
+plt.xlim(90, 270)
 plt.ylim(-400, 400)
 plt.draw()
-plt.savefig("plot-spline-"+str(set_ball_angle)+".png")
+plt.savefig("plot-spline-"+str(set_ball_y)+".png")
 plt.pause(2)
 plt.close()
+
 
 # sample prediction
 spline_model.fit(X, y)
